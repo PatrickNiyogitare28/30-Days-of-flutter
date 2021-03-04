64233030ea27affeda8f688f9575e251e4ac2549
Patrick <patrickniyogitare28@gmail.com>
2021-03-04 07:26:52 +0200

    chore: developed a basic UI

--- expensises_tracker/lib/main.dart
+++ expensises_tracker/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -20,19 +22,18 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transaction = [
+  final List<Transaction> transactions = [
     Transaction(
-      id: 't1', 
-      title:'New shoes', 
-      amount: 69.05, 
-      date: DateTime.now()),
+        id: 't1', title: 'New shoes', amount: 69.05, date: DateTime.now()),
     Transaction(
-      id: 't2', 
-      title:'Weekly Groceries', 
-      amount: 100, 
-      date: DateTime.now())
-
+        id: 't2', title: 'Weekly Groceries', amount: 100, date: DateTime.now())
   ];
+
+  // String titleInput;
+  // String amountInput;
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -41,20 +42,81 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter app'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        // mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Card(
-            child: Container(
-              color: Colors.blue,
+          Container(
               width: double.infinity,
-              child: Text('CHART')),
-              elevation: 5,
-          ),
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART'),
+                elevation: 5,
+              )),
           Card(
-            color: Colors.red,
-            child: Text('LIST OF TX'),
-          )
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                    // onChanged: (val) {titleInput = val;},
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                    // onChanged: (val) {amountInput = val;},
+                  ),
+                  FlatButton(
+                    child: Text('Add Transaction'),
+                    onPressed: () {
+                      // print('title:  '+titleInput+' amount: '+amountInput);
+                    },
+                    textColor: Colors.purple,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Column(
+              children: transactions.map((tx) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '\$${tx.amount}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        DateFormat().add_yMMMd().format(tx.date),
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            );
+          }).toList()),
         ],
       ),
     );
