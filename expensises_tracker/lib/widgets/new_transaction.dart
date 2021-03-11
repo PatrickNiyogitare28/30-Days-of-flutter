20a0b9b61e71f275c988823b8085ec4c0b6fabae
Patrick <patrickniyogitare28@gmail.com>
2021-03-11 20:26:58 +0200

    chore: added model #fit-transaction-#expensises_tracker

--- expensises_tracker/lib/widgets/new_transaction.dart
+++ expensises_tracker/lib/widgets/new_transaction.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import './user_transaction.dart';
 
 class NewTransaction extends StatelessWidget {
   final Function addTx;
@@ -8,6 +7,19 @@ class NewTransaction extends StatelessWidget {
   final amountController = TextEditingController();
 
   NewTransaction(this.addTx);
+
+  void submitData(){
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if(enteredTitle.isEmpty || enteredAmount < 0){
+      return;
+    }
+
+    addTx(
+      titleController.text, double.parse(amountController.text)
+     );
+  }
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -21,20 +33,19 @@ class NewTransaction extends StatelessWidget {
               decoration: InputDecoration(labelText: 'Title'),
               // onChanged: (val) {titleInput = val;},
               controller: titleController,
+              onSubmitted: (_) => submitData(),
+
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
+              keyboardType: TextInputType.number,
               // onChanged: (val) {amountInput = val;},
               controller: amountController,
+              onSubmitted: (_) => submitData(),
             ),
             FlatButton(
               child: Text('Add Transaction'),
-              onPressed: () {
-                // print('title:  '+titleInput+' amount: '+amountInput);
-                // print('Title: '+titleController.text+" amount: "+amountController.text);
-                addTx(
-                    titleController.text, double.parse(amountController.text));
-              },
+              onPressed: submitData,
               textColor: Colors.purple, 
             )
           ],
