b546c1593c9933e61088bf58156d4560332b2cf5
Patrick <patrickniyogitare28@gmail.com>
2021-04-30 17:02:23 +0200

    chore: rendered chart bar #05:45

--- expensises_tracker/lib/widgets/chart.dart
+++ expensises_tracker/lib/widgets/chart.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -25,13 +26,20 @@ class Chart extends StatelessWidget {
         'amount':totalSum};
     });
   }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item){
+      return sum + item['amount'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(children: groupedTransactionValues.map((data){
-        return Text('${data['day']}: ${data['amount']}' );
+        return ChartBar(data['day'], data['amount'], (data['amount'] as double)/totalSpending);
       }).toList(),
 
     ),
