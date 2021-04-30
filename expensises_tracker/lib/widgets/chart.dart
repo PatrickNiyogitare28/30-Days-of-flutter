1d1236e288e15d2785ff2642b770725a04bec2f9
Patrick <patrickniyogitare28@gmail.com>
2021-04-30 17:02:23 +0200

    bug: fixed rendering chart with 0.0

--- expensises_tracker/lib/widgets/chart.dart
+++ expensises_tracker/lib/widgets/chart.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -9,7 +10,7 @@ class Chart extends StatelessWidget {
   Chart(this.recentTransactions);
   
   List<Map<String, Object>> get groupedTransactionValues{
-    return  List.generate(7, (index) {
+    return  List.generate(7, (index) { 
       final weekDay = DateTime.now().subtract(Duration(days: index),);
       double totalSum=0.0;
       for(var i = 0; i < recentTransactions.length; i++){
@@ -25,13 +26,23 @@ class Chart extends StatelessWidget {
         'amount':totalSum};
     });
   }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item){
+      return sum + item['amount'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(children: groupedTransactionValues.map((data){
-        return Text('${data['day']}: ${data['amount']}' );
+        return ChartBar(
+          data['day'],
+          data['amount'], 
+          totalSpending==0.0 ? 0.0 : (data['amount'] as double)/totalSpending);
       }).toList(),
 
     ),
