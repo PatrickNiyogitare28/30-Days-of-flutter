a8ff2b1d614017ffea055dfa07c0585e6aef68f3
Patrick <patrickniyogitare28@gmail.com>
2021-04-30 23:14:36 +0200

    fit: added ListTile

--- expensises_tracker/lib/widgets/transaction_list.dart
+++ expensises_tracker/lib/widgets/transaction_list.dart
@@ -26,41 +26,22 @@ class TransactionList extends StatelessWidget {
       ],) :
       ListView.builder(
             itemBuilder: (context, index) {
-              return  Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                        color: Theme.of(context).primaryColor,
-                        width: 2,
-                      )),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${transactions[index].amount}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          transactions[index].title,
-                          style: Theme.of(context).textTheme.title,
-                        ),
-                        Text(
-                          DateFormat().add_yMMMd().format(transactions[index].date),
-                          style: TextStyle(color: Colors.grey),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              );
+             return Card(
+                 margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                 child: ListTile(
+                 leading: CircleAvatar(
+                   radius: 30,
+                   child:  Padding(
+                     padding: const EdgeInsets.all(6 ),
+                     child: FittedBox
+                      (child: Text('\$${transactions[index].amount}')),
+                   ),),
+                   title: Text(
+                     transactions[index].title,
+                      style: Theme.of(context).textTheme.title),
+                      subtitle: Text(DateFormat.yMMMd().format(transactions[index].date)),
+               ),
+             );
             },
             itemCount: transactions.length,
             // children: transactions.map((tx) {
